14f71d0f3266014238c0fcf5dae8d92bc36879ff
J Wokaty <jwokaty@users.noreply.github.com>
2021-07-06 21:57:20 -0400

    Remove RStudio from s6 services

--- scripts/install.sh
+++ scripts/install.sh
@@ -8,15 +8,7 @@ apt-get update
 apt-get install -y --no-install-recommends npm nodejs libzmq3-dev
 npm install -g configurable-http-proxy
 rm -rf ~/.npm
-python3 -m pip install jupyterlab
-
-# Install repositories to facilitate Jupyterlab proxying RStudio
-for repo in jupyter-server-proxy jupyter-rsession-proxy
-do
-  git clone https://github.com/jupyterhub/$repo.git /tmp/$repo
-  python3 -m pip install /tmp/$repo
-done
-rm -rf /tmp/jupyter*
+python3 -m pip install jupyterlab jupyter-server-proxy jupyter-rsession-proxy
 
 # Install Python analysis dependencies
 python3 -m pip install pandas \
@@ -37,13 +29,15 @@ echo "waldronlab:$PASSWORD" | chpasswd
 chmod -R g+w /var/lib/rstudio-server
 chown -R :rstudio /var/lib/rstudio-server
 
+# Remove s6 Rstudio service
+rm -rf /etc/services.d/rstudio
+
 # Create Jupyter configuration file
 cd /home/waldronlab
 su -c "jupyter server --generate-config" waldronlab
 
 # Move script to s6 initialization script path
 mv /tmp/userhashconf.py /etc/cont-init.d/
-chmod 755 /etc/cont-init.d/userhashconf.py
 
 # Clone repository and change permissions waldronlab
 git clone https://github.com/waldronlab/$REPOSITORY.git /home/waldronlab/$REPOSITORY
