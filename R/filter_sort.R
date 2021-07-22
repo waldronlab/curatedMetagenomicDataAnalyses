2bf4fc388866df1f4fa72916205b0a0d2cf53fba
d-golzato <d.golzato@gmail.com>
2021-07-22 17:40:18 +0200

    Added functions filter_sort() and runMetaanalysis_categorical and runMetaanalysis_quantitative to R/ folder; added roxygen documentation to man/ folder; changed  combined_metadata to sampleMetadata

---
+++ R/filter_sort.R
@@ -0,0 +1,51 @@
+#' Filter and Sort Features in Meta-analysis final data-frame 
+#' This function allows to remove from the dataframe with the meta-analysis 
+#' results those features for which the Standardized Mean Difference couldn't
+#' be computed in at least a user-specified number of cohorts (na_filter).
+#' 
+#' Additionally, this function orders in increasing order the features according
+#' to the corrected p-values and in decreasing order for the Overall effect size
+#' 
+#' @param df_to_sort The dataframe containing the meta-analysis results (final_df
+#' in this vignette).
+#' 
+#' @param na_filter Maximum number of cohorts for which the SDM difference couldn't
+#' be computed to allow for a feature to be included.
+#'  
+#'
+#' @return The dataframe with the meta-analysis results, filtered for prevalent
+#' features and ordered by adj.p-value and effect size.
+#' @importFrom dplyr %>% arrange select
+
+
+
+
+
+filter_sort <- function(df_to_sort, na_filter){
+  cols_cohenD <- grep("CohenD|Correlation",colnames(df_to_sort))
+  cols_cohenD <- cols_cohenD[which(cols_cohenD >= 9)]
+  df_to_sort$na_n <- apply(df_to_sort[,cols_cohenD], MARGIN = 1, FUN = function(x){
+    na_n <- sum(!is.na(x))
+  })
+  
+  df_to_sort <- df_to_sort[which(df_to_sort$na_n >= na_filter),]
+  df_to_sort$RE_correlation_ABS <- abs(as.double(df_to_sort[,1]))
+  df_to_sort$lower_than_fdr <- ifelse(df_to_sort$FDR_Qvalue < 0.2, 1, 0)
+  
+  #Split the dataset
+  df_to_sort_lower_fdr <- df_to_sort[which(df_to_sort$lower_than_fdr == 1),]
+  df_to_sort_upper_fdr <- df_to_sort[which(df_to_sort$lower_than_fdr == 0),]
+  
+  #Sort according to ABS.re
+  df_to_sort_lower_fdr <- df_to_sort_lower_fdr %>% 
+    arrange(-RE_correlation_ABS)
+  
+  df_to_sort_upper_fdr <- df_to_sort_upper_fdr %>% 
+    arrange( -RE_correlation_ABS)
+  
+  df_to_sort <- rbind(df_to_sort_lower_fdr, df_to_sort_upper_fdr)
+  df_to_sort <- df_to_sort %>% 
+    select(-c(na_n, lower_than_fdr,RE_correlation_ABS))
+  
+  return(df_to_sort)
+} 
---
+++ R/runMetaanalysis_quantitative.R
@@ -0,0 +1,35 @@
+#' Title Single Species Meta-analysis (Quantitative Variables)
+#' Wrapping function to perform a meta-analysis using function \emph{metagen} 
+#' from package \emph{meta} for a single Species. Between-study variance is
+#' computed with Paule-Mandel estimator (method.tau = PM). The summary measure
+#' is the Standardize Mean Difference (sm = ZCOR)
+#' @param R_vector A vector with correlations for a  
+#' single feature (Species) across datasets. 
+#' @param n A vector with the number of samples in each cohort 
+#'
+#'  
+#'
+#' @return A named vector with the results of the meta-analysis for the feature
+#' implicitly described by d_vector and SE_d_vector.  
+#' RE_Correlation","SE_RE","CI_RE","Zscore","p-value","tau2","I^2"
+#' @importFrom meta metagen
+#' @export
+
+runMetaanalysis_quantitative <- function(R_vector, n) {
+  a <- meta::metagen(TE=d_vector,
+                     seTE=SE_d_vector,
+                     studlab=rownames(d_vector),
+                     method.tau="PM",          
+                     sm="ZCOR")
+  
+  final_vector <-c(a$TE.random,
+                   a$seTE.random,
+                   paste(a$lower.random,a$upper.random,sep=";"),
+                   a$zval.random,
+                   a$pval.random,
+                   a$tau2,
+                   a$I2)
+  
+  names(final_vector) <- c("RE","SE_RE","CI_RE","Zscore","p-value","tau2","I^2")
+  return(final_vector)
+}
